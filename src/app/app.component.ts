6724bccb5392e63e644164f267b282188c4b17ef
Fawad <fawadmueed@gmail.com>
2018-04-13 00:53:26 -0400

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,44 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { MarketTableComponent } from './market-table/market-table.component';
 import { HttpModule } from '@angular/http';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+
+import { WelcomeComponent } from './welcome/welcome.component';
+import { MarketTableComponent } from './market-table/market-table.component';
+import { ContactComponent } from './contact/contact.component'
+import { RouterModule } from '@angular/router';
+import { NotFoundComponentComponent } from './not-found-component/not-found-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MarketTableComponent
+    MarketTableComponent,
+    WelcomeComponent,
+    ContactComponent,
+    NotFoundComponentComponent,
+    
 
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { 
+        path : '' , component : WelcomeComponent 
+      },
+      { 
+        path : 'markets' , component : MarketTableComponent 
+      },
+      {
+         path : 'contact' , component : ContactComponent 
+      },
+      {
+        path : '**' , component : NotFoundComponentComponent 
+     }
+    ])
   ],
   providers: [DataApiService],
   bootstrap: [AppComponent],
